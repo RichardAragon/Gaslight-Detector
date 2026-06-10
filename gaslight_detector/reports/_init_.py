4ff08bb14f6a077e3f50698247de2f2062de76ee
RichardAragon <138004861+RichardAragon@users.noreply.github.com>
2026-06-10 09:57:27 -0700

    Initialize reports module with exports

---
+++ gaslight_detector/reports/_init_.py
@@ -0,0 +1,6 @@
+from gaslight_detector.reports.json_report import write_json
+from gaslight_detector.reports.markdown import render_markdown
+from gaslight_detector.reports.html import render_html
+from gaslight_detector.reports.plots import write_plots
+
+__all__ = ["write_json", "render_markdown", "render_html", "write_plots"]
