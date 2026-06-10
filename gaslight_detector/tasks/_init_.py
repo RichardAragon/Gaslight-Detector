26f6e3005c9292ad62f2f160ec77483b5f1bf44d
RichardAragon <138004861+RichardAragon@users.noreply.github.com>
2026-06-10 10:11:11 -0700

    Initialize tasks module with imports and exports

---
+++ gaslight_detector/tasks/_init_.py
@@ -0,0 +1,4 @@
+from gaslight_detector.tasks.schema import TaskCard, Frame, load_task
+from gaslight_detector.tasks.validators import validate_task, InvarianceReport
+
+__all__ = ["TaskCard", "Frame", "load_task", "validate_task", "InvarianceReport"]
